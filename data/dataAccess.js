401b045fe0b00e0f8026901708f4107906b8b2ba
Ciaran Phillips <ciaran@ciaranphillips.com>
2016-08-28 21:59:03 +0000

    fix indentation and formatting of dataAccess.js (no functionality changes)
    Use beautify-js to bring in line with standard formatting

--- data/dataAccess.js
+++ data/dataAccess.js
@@ -1,32 +1,30 @@
-(function() {
-  var fs = require('fs');
-  var airportFile = '/airports.json';
-  var airportData = null;
+(function () {
+    var fs = require('fs');
+    var airportFile = '/airports.json';
+    var airportData = null;
 
-  exports.loadAirportList = function(callback) {
-    if (airportData != null) {
-      loadFromCache(callback);
+    exports.loadAirportList = function (callback) {
+        if (airportData != null) {
+            loadFromCache(callback);
+        } else {
+            loadFromFile(callback);
+        }
+    };
+
+    function loadFromCache() {
+        setTimeout(() => {
+            return callback(false, airportData);
+        }, 0);
     }
-    else {
-      loadFromFile(callback);
+
+    function loadFromFile(callback) {
+        fs.readFile(__dirname + airportFile, function (err, data) {
+            if (err) {
+                callback(err, data);
+            } else {
+                callback(false, JSON.parse(data));
+            }
+        });
     }
-  };
-
-  function loadFromCache() {
-    setTimeout(() => {
-      return callback(false, airportData);
-    }, 0);
-  }
-
-  function loadFromFile(callback) {
-    fs.readFile(__dirname + airportFile, function(err, data) {
-      if (err) {
-        callback(err, data);
-      }
-      else {
-        callback(false, JSON.parse(data));
-      }
-    });
-  }
 
 }());
