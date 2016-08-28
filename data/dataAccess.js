7caf1f749573b716acc8b2993a1fc06099b514e9
Ciaran Phillips <ciaran@ciaranphillips.com>
2016-08-28 22:07:38 +0000

    update module structure of dataAccess.js to be consistent with other modules
    - use a single module.exports statement at the top of the file
    - replace magic strings with defined constant
    - prefer const and let assignments over var

--- data/dataAccess.js
+++ data/dataAccess.js
@@ -1,30 +1,40 @@
-(function () {
-    var fs = require('fs');
-    var airportFile = '/airports.json';
-    var airportData = null;
+const fs = require('fs');
 
-    exports.loadAirportList = function (callback) {
-        if (airportData != null) {
-            loadFromCache(callback);
+
+module.exports = {
+    loadAirportList
+};
+
+
+
+const AIRPORT_FILE = '/airports.json';
+
+
+let airportData = null;
+
+
+function loadAirportList(callback) {
+    if (airportData != null) {
+        loadFromCache(callback);
+    } else {
+        loadFromFile(callback);
+    }
+};
+
+
+function loadFromCache() {
+    setTimeout(() => {
+        return callback(false, airportData);
+    }, 0);
+}
+
+
+function loadFromFile(callback) {
+    fs.readFile(__dirname + AIRPORT_FILE, function (err, data) {
+        if (err) {
+            callback(err, data);
         } else {
-            loadFromFile(callback);
+            callback(false, JSON.parse(data));
         }
-    };
-
-    function loadFromCache() {
-        setTimeout(() => {
-            return callback(false, airportData);
-        }, 0);
-    }
-
-    function loadFromFile(callback) {
-        fs.readFile(__dirname + airportFile, function (err, data) {
-            if (err) {
-                callback(err, data);
-            } else {
-                callback(false, JSON.parse(data));
-            }
-        });
-    }
-
-}());
+    });
+}
