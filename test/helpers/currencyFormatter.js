7c118198105b784f62013c6fe8d9045139c0020d
Ciaran Phillips <ciaran@ciaranphillips.com>
2016-09-11 18:13:41 +0000

    Start work on currency formatter tests

---
+++ test/helpers/currencyFormatter.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+describe('CurrencyFormatter', function () {
+    describe('#format()', function () {
+        const CurrencyFormatter = require('../../api/helpers/currencyFormatter.js');
+        const formatter = new CurrencyFormatter();
+
+    });
+});
+        
