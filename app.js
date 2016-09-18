4e8a6fcea3d68a2de87d304ed2e9905e10cd0b38
Ciaran Phillips <ciaranphillips1@gmail.com>
2016-09-18 19:03:11 +0100

    Add endpoints for activity API calls

--- app.js
+++ app.js
@@ -106,6 +106,46 @@ function serve(config) {
         });
     });
 
+
+    app.get('/activities/:location', function(req, res) {
+        const GetActivitiesAction = require('./expedia/getActivitiesAction.js');
+        const action = new GetActivitiesAction();
+
+        const options = {
+            location: req.params.location,
+            apikey: config.expediaApiKey
+        };
+        action.getActivities(options, function(err, data) {
+            if (err) {
+                console.log(err);
+                res.send('something went wrong');
+                return;
+            }
+            res.header('Access-Control-Allow-Origin', '*');
+            res.send(data);
+        });
+    });
+
+    app.get('/activity/:id', function(req, res) {
+        const ActivityDetails = require('./expedia/activityDetailsAction.js');
+        const action = new ActivityDetails();
+
+        const options = {
+            activityId: req.params.id,
+            apikey: config.expediaApiKey
+        };
+        action.getActivityDetails(options, function(err, data) {
+            if (err) {
+                console.log(err);
+                res.send('something went wrong');
+                return;
+            }
+            res.header('Access-Control-Allow-Origin', '*');
+            res.send(data);
+
+        });
+    })
+
     app.listen(4000, '0.0.0.0', function () {
         console.log('App listening on port 4000');
     });
